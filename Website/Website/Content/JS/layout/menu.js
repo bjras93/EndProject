4e167d53b50261920323f97aee791929504a5314
bjras93 <br@c-tilsted.dk>
2016-09-07 11:06:10 +0200

    Day 3 v1.1

--- Website/Website/Content/JS/layout/menu.js
+++ Website/Website/Content/JS/layout/menu.js
@@ -1,7 +1,36 @@
 ﻿var menu = {
     init: function () {
+        menu.mood();
+        menu.account();
+    },
+    mood: function () {        
         $('#mood').on('click', function () {
             $('.mood').toggle("slide", { direction: "right" }, 1000);
         });       
+    },
+    account: function () {
+        var prev = 'account-details',
+            loc = location.href.split('?m=')[1];
+        if (loc == null) {
+            $('.' + prev).show();
+        }
+        $('.account-menu ul li a').each(function () {
+            var $this = $(this),
+                select = $('.' + $this.data('section'));
+                if (loc != null)
+                {
+                    select.hide();
+                    $('.account-' + loc).show();
+                }
+
+            $this.on('click', function (e) {
+                $('.' + prev).hide();
+                select.show();
+                prev = $this.data('section');
+                location.href = '?m=' + prev.replace('account-', '');
+                e.preventDefault();
+
+            });
+        });
     }
 }
